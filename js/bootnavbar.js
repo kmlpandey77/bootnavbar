d0d045fe8c6d25989379fe2d87ded85731d2b96c
Kamal Pandey <kml.pandey77@gmail.com>
2019-02-15 18:31:07 +0545

    responsive fix

--- js/bootnavbar.js
+++ js/bootnavbar.js
@@ -1,13 +1,39 @@
 (function($) {
+    var defaults={
+        sm : 540,
+        md : 720,
+        lg : 960,
+        xl : 1140,
+        navbar_expand: 'lg'
+    };
     $.fn.bootnavbar = function() {
-        $(this).find('.dropdown').hover(function() {
-            $(this).addClass('show');
-            $(this).find('.dropdown-menu').first().addClass('show').addClass('animated fadeIn').one('animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd', function () {
-            	$(this).removeClass('animated fadeIn');      	
+
+        var screen_width = $(document).width();
+
+        if(screen_width >= defaults.lg){
+            $(this).find('.dropdown').hover(function() {
+                $(this).addClass('show');
+                $(this).find('.dropdown-menu').first().addClass('show').addClass('animated fadeIn').one('animationend oAnimationEnd mozAnimationEnd webkitAnimationEnd', function () {
+                    $(this).removeClass('animated fadeIn');
+                });
+            }, function() {
+                $(this).removeClass('show');
+                $(this).find('.dropdown-menu').first().removeClass('show');
             });
-        }, function() {
-            $(this).removeClass('show');
-            $(this).find('.dropdown-menu').first().removeClass('show');
+        }
+
+        $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+          if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+          }
+          var $subMenu = $(this).next(".dropdown-menu");
+          $subMenu.toggleClass('show');
+
+          $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+            $('.dropdown-submenu .show').removeClass("show");
+          });
+
+          return false;
         });
-    };    
+    };
 })(jQuery);
