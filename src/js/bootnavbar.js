37cb6dd6eda7446013a2a6dbc47ee6ce483397a7
Kamal Pandey <kmlpandey77@gmail.com>
2022-05-05 20:05:23 +0545

    code refactor and add transitions

---
+++ src/js/bootnavbar.js
@@ -0,0 +1,46 @@
+function bootnavbar(el = 'main_navbar', options){
+    let defaultOption  ={
+
+    }
+
+    options = {...defaultOption, ...options }
+
+
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    this.init = function(){
+        var dropdowns = document.getElementById(el).getElementsByClassName("dropdown");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var dropdown = dropdowns.item(i);
+            dropdown.addEventListener("mouseover", function(){
+                this.classList.add('show');
+                this.getElementsByClassName("dropdown-menu")[0].classList.add('show');
+            });
+            dropdown.addEventListener("mouseout", function(){
+                this.classList.remove('show');
+                this.getElementsByClassName("dropdown-menu")[0].classList.remove('show');
+            });
+        }       
+    }
+
+    this.init();    
+}
